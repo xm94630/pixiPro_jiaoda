f2d789ddf630765a5e2806524a2ceda164ae87d1
xm94630 <xm94630@126.com>
2018-09-19 16:30:59 +0800

    clean-webpack-plugin 使用，用来清除dist

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,6 +10,7 @@ module.exports = {
   },
   mode:"production",
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: '新交大群侠传',
       template: 'index.html'
